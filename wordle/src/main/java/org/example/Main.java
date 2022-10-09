d13682cc819ad165663c397bd527c7cae96a4d23
Shntel Thomas <shantelthomas2023@gmail.com>
2022-10-09 00:21:53 -0400

    unoptimized finishes version of the wordle

--- wordle/src/main/java/org/example/Main.java
+++ wordle/src/main/java/org/example/Main.java
@@ -1,7 +1,75 @@
 package org.example;
 
+import java.util.Scanner;
+
+import static org.example.Util.Result.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Generate random word from list
+        String randWord = Util.getRandomWord();
+        int counter = 1;
+        boolean winner = false;
+
+        //Print generate message
+        System.out.println("**********************************");
+        System.out.println("******* WELCOME TO SHAN WORDLE ******");
+        System.out.println("**********************************");
+        System.out.println();
+
+        String userWord = null;
+        while (counter != 7) {
+
+            //take user input
+            Scanner input = new Scanner(System.in);
+            System.out.println();
+            System.out.println("Type your 5 letter word (attempt " + counter + " of 6):");
+            userWord = input.nextLine().toLowerCase();
+
+
+            //check if input is valid
+            if (userWord.length() == 5) {
+                //check each index letter against each other
+                CompareIndexes(randWord, userWord);
+                counter += 1;
+            } else {
+                System.out.println("Word needs to be 5 letters");
+            }
+        }
+        System.out.println();
+        System.out.println( "The word was " + randWord);
+    }
+
+    private static void CompareIndexes(String randWord, String userWord) {
+        //ArrayList<String> result = new ArrayList<>();
+        for(int i = 0; i <= randWord.length()-1; i++){
+            String TemptChar = String.valueOf(userWord.charAt(i));
+            if(userWord.charAt(i) == randWord.charAt(i)){
+                System.out.print(Util.getFormattedLetter(userWord.charAt(i), HIT));
+            }
+            else if(randWord.contains(TemptChar) == true) {  //optimise to account for letters that were already
+                System.out.print(Util.getFormattedLetter(userWord.charAt(i), SEMI_HIT));
+            }
+            else if(randWord.contains(TemptChar) == false) {
+                System.out.print(Util.getFormattedLetter(userWord.charAt(i), MISS));
+            }
+        }
+    }
+
+    private static int checkUserInput(String userWord, int counter) {
+        if(userWord.length()== 5){
+            return  counter+=1;
+        }
+        else {
+            System.out.println("Word needs to be 5 letters");
+        }
+        return counter;
     }
 }
+
+//Generate random word from list  - lover
+//input 5 letterword from user -hoads
+//check each index letter against each other
+//fif the leter in have same index return Hit,
+// If not same but in work return Semi_hit,
+// if not in word return Miss
